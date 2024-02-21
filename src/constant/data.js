76826e314099af8f9f14f2113374a1fbe8ee2ff4
Vikramsaini26 <Vikramsaini@Mac-mini.local>
2024-02-21 20:50:01 +0530

    feat: complete tip calculator app

--- src/constant/data.js
+++
@@ -1,8 +0,0 @@
-export const defaultTipState = [
-  { percentage: "5%", isCustom: false, isClick: false },
-  { percentage: "10%", isCustom: false, isClick: false },
-  { percentage: "15%", isCustom: false, isClick: false },
-  { percentage: "25%", isCustom: false, isClick: false },
-  { percentage: "50%", isCustom: false, isClick: false },
-  { percentage: "Custom", isCustom: false, isClick: false },
-];
